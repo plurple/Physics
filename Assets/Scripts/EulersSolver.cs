82c025517f5a10ffd224623a053977f64e016101
plurple <mattgrierson@btinternet.com>
2020-10-18 16:21:43 +0100

    Part 1 Trajectory

---
+++ Assets/Scripts/EulersSolver.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EulersSolver : MonoBehaviour
+{
+    public Vector3 initialPosition;
+    public Vector3 velocity;
+    public Vector3 acceleration;
+    public float time = 0.0f;
+
+    private void Start()
+    {
+        time += 1.0f / 60.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EulerSolver();
+    }
+
+    public void EulerSolver()
+    {
+        velocity = velocity + acceleration * time;
+        Vector3 newPosition = gameObject.transform.position + velocity * time;
+        gameObject.transform.position = newPosition;
+    }
+}
---
+++ Assets/Scripts/NewtonsTrajectory.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewtonsTrajectory : MonoBehaviour
+{
+    public Vector3 initialPosition;
+    public Vector3 velocity;
+    public Vector3 acceleration;
+    public float time = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += 1.0f / 60.0f;
+        NewtonsLaw();
+    }
+
+    public void NewtonsLaw()
+    {
+        Vector3 newPosition = new Vector3(0.0f, 0.0f, 0.0f);
+        for(int i = 0; i < 3; i++)
+        {
+            newPosition[i] = initialPosition[i] + (velocity[i] * time) + ((acceleration[i] * time * time) / 2.0f);
+        }
+
+        gameObject.transform.position = newPosition;
+    }
+}
